ad9598da819c594d7337e95dde70df2059a1b15e
karl <karl.van.rompaey@hotmail.com>
2019-04-08 15:15:31 +0100

    Initial commit

---
+++ rollup.config.dev.js
@@ -0,0 +1,23 @@
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import json from 'rollup-plugin-json'
+
+module.exports = [
+  // add config for every entry point
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true
+    },
+    plugins: [
+      resolve(),
+      babel({
+        configFile: './babel.config.js',
+        exclude: 'node_modules/**' // only transpile our source code
+      }),
+      json()
+    ]
+  }
+]
---
+++ rollup.config.prod.js
@@ -0,0 +1,22 @@
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import json from 'rollup-plugin-json'
+
+module.exports = [
+  // add config for every entry point
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/index.js',
+      format: 'cjs'
+    },
+    plugins: [
+      resolve(),
+      babel({
+        configFile: './babel.config.js',
+        exclude: 'node_modules/**' // only transpile our source code
+      }),
+      json()
+    ]
+  }
+]
